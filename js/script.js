742657e5fb54a225b51fbdc3c944e82d261ae6dc
hyunjaeha <gkguswo1231@gmail.com>
2026-05-07 17:14:52 +0900

    work-260507

--- js/script.js
+++ js/script.js
@@ -20,3 +20,70 @@ mbCloseBtn.addEventListener("click", (e) => {
   mbCloseBtn.style.display = "none";
   mbMenulist.style.display = "none";
 });
+
+// 무드겟 이미지 슬라이드
+const moodgetImages = [
+  "assets/images/moodget_splash.png",
+  "assets/images/moodget_onboarding.png",
+  "assets/images/moodget_home.png",
+];
+
+const moodgetScreens = document.querySelectorAll(".moodget-screen");
+
+let moodgetIndex = 0;
+let activeMoodget = 0;
+
+moodgetImages.forEach((src) => {
+  const img = new Image();
+  img.src = src;
+});
+
+if (moodgetScreens.length === 2) {
+  setInterval(() => {
+    const currentScreen = moodgetScreens[activeMoodget];
+    const nextScreen = moodgetScreens[1 - activeMoodget];
+
+    moodgetIndex = (moodgetIndex + 1) % moodgetImages.length;
+
+    nextScreen.src = moodgetImages[moodgetIndex];
+
+    currentScreen.classList.remove("active");
+    nextScreen.classList.add("active");
+
+    activeMoodget = 1 - activeMoodget;
+  }, 3000);
+}
+
+// 쩝쩝박사 이미지 슬라이드
+const jjupImages = [
+  "assets/images/splash.png",
+  "assets/images/home.png",
+  "assets/images/map.png",
+  "assets/images/character.png",
+];
+
+const jjupScreens = document.querySelectorAll(".jjup-screen");
+
+let jjupIndex = 0;
+let activeJjup = 0;
+
+jjupImages.forEach((src) => {
+  const img = new Image();
+  img.src = src;
+});
+
+if (jjupScreens.length === 2) {
+  setInterval(() => {
+    const currentScreen = jjupScreens[activeJjup];
+    const nextScreen = jjupScreens[1 - activeJjup];
+
+    jjupIndex = (jjupIndex + 1) % jjupImages.length;
+
+    nextScreen.src = jjupImages[jjupIndex];
+
+    currentScreen.classList.remove("active");
+    nextScreen.classList.add("active");
+
+    activeJjup = 1 - activeJjup;
+  }, 3000);
+}
